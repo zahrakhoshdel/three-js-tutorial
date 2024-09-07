07a1009a72233d6ad52d7ad97753793d57dd6e0a
unknown <imzahrakhoshdel@gmail.com>
2024-09-07 19:53:46 +0330

    setting up - adding box geometry

---
+++ 03-dat-gui/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+import * as THREE from "three";
+
+import SceneInit from "./lib/SceneInit";
+
+function App() {
+  useEffect(() => {
+    const test = new SceneInit("myThreeJsCanvas");
+    test.initialize();
+    test.animate();
+
+    const boxGeometry = new THREE.BoxGeometry(24, 24, 24);
+    const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    test.scene.add(boxMesh);
+  }, []);
+
+  return (
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
+  );
+}
+
+export default App;
