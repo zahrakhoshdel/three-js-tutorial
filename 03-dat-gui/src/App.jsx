b8ec5622b89a944c391020a634c195d86b27a4f1
unknown <imzahrakhoshdel@gmail.com>
2024-09-07 21:34:08 +0330

    updating material - color & wireframe

--- 03-dat-gui/src/App.jsx
+++ 03-dat-gui/src/App.jsx
@@ -19,12 +19,22 @@ function App() {
     //initialize
     const gui = new GUI();
 
+    // Changing Geometry (scale, rotation)
     gui.add(boxMesh.rotation, "x", 0, Math.PI).name("Rotate X Axis");
     gui.add(boxMesh.rotation, "y", 0, Math.PI).name("Rotate Y Axis");
     gui.add(boxMesh.rotation, "z", 0, Math.PI).name("Rotate Z Axis");
     gui.add(boxMesh.scale, "x", 0, 2).name("Scale X Axis");
     gui.add(boxMesh.scale, "y", 0, 2).name("Scale Y Axis");
     gui.add(boxMesh.scale, "z", 0, 2).name("Scale Z Axis");
+
+    //updating material (color & wireframe)
+    const materialParams = {
+      boxMeshColor: boxMesh.material.color.getHex(),
+    };
+    gui.add(boxMesh.material, "wireframe");
+    gui
+      .addColor(materialParams, "boxMeshColor")
+      .onChange((value) => boxMesh.material.color.set(value));
   }, []);
 
   return (
