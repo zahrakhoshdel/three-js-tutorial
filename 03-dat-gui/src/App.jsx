a411fb39c7872b4e2dd7e8b6378f99e255ca62a6
unknown <imzahrakhoshdel@gmail.com>
2024-09-07 21:19:27 +0330

    updating basic parameters - rotation & scale

--- 03-dat-gui/src/App.jsx
+++ 03-dat-gui/src/App.jsx
@@ -18,7 +18,13 @@ function App() {
 
     //initialize
     const gui = new GUI();
-    
+
+    gui.add(boxMesh.rotation, "x", 0, Math.PI).name("Rotate X Axis");
+    gui.add(boxMesh.rotation, "y", 0, Math.PI).name("Rotate Y Axis");
+    gui.add(boxMesh.rotation, "z", 0, Math.PI).name("Rotate Z Axis");
+    gui.add(boxMesh.scale, "x", 0, 2).name("Scale X Axis");
+    gui.add(boxMesh.scale, "y", 0, 2).name("Scale Y Axis");
+    gui.add(boxMesh.scale, "z", 0, 2).name("Scale Z Axis");
   }, []);
 
   return (
